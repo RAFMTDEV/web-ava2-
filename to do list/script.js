748319ce44d7533f3bb22a9154e98d1f53d9bce2
RAFMTDEV <143976816+RAFMTDEV@users.noreply.github.com>
2023-10-17 22:54:45 -0300

    Add files via upload
    lista de tarefas

---
+++ to do list/script.js
@@ -0,0 +1,16 @@
+function adicionar(){
+
+    let text =  document.getElementById("texto").value;
+    let list = document.getElementById("lista").innerHTML;
+    
+    list += "<li>" +text+ "</li>";
+
+    document.getElementById("lista").innerHTML = list;
+    
+    document.getElementById("texto").value = null;
+}
+
+function excluir(){
+    Input.value = " ";
+
+}
